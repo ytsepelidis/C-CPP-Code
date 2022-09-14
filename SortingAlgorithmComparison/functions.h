8262bf7bae272175961cb73383cf1917b041d8ed
ytsepelidis <102433126+ytsepelidis@users.noreply.github.com>
2022-09-14 19:39:02 +0300

    Create functions.h

---
+++ SortingAlgorithmComparison/functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define n_max 100
+
+int SortingAlgorithmA(int n, int* A);
+int SortingAlgorithmB(int n, int* B);
+void PrintArray(int n, int* arr);
+void CheckingArrays(int n, int* A, int* B);
